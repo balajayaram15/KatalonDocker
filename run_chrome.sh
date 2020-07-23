904330b468a7fddab0903945ba5d0db1f10c207c
balajayaram15 <68600655+balajayaram15@users.noreply.github.com>
2020-07-23 08:23:59 +0530

    Update run_chrome.sh

--- run_chrome.sh
+++ run_chrome.sh
@@ -4,4 +4,4 @@ rm -rfv ./reports/chrome
 mkdir -p ./reports/chrome
 
 katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/FirstTest"'
-docker run --rm -v $(pwd)/KatalonDocker:/katalon/katalon/source:ro -v $(pwd)/reports/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
+docker run --rm -v $(pwd)/KatalonDocker:/katalon/katalon/source:ro -v $(pwd)/reports/chrome:/katalon/katalon/report -apiKey="24a81f22-b024-47ed-ae1a-7394a8d24143" -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
